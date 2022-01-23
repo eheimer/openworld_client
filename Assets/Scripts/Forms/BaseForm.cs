d5545889da6df4f9040f6b7abb46091179bf4209
Eric Heimerman <eric@heimerman.org>
2022-01-23 16:49:15 -0600

    testing discord integration

--- Assets/Scripts/Forms/BaseForm.cs
+++ Assets/Scripts/Forms/BaseForm.cs
@@ -4,7 +4,7 @@ using Proyecto26;
 using Openworld.Models;
 using Openworld.Scenes;
 
-namespace Openworld.Forms
+namespace Openworld.Forms //testing discord integration
 {
   public abstract class BaseForm : BaseScene
   {
