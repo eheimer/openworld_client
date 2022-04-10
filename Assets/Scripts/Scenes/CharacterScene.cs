123f668066b240618470b174759a42b9c67c5d20
Eric Heimerman <eric@heimerman.org>
2022-04-10 14:10:03 -0500

    added code to load character detail from server when the character scene starts

--- Assets/Scripts/Scenes/CharacterScene.cs
+++ Assets/Scripts/Scenes/CharacterScene.cs
@@ -1,11 +1,32 @@
+using Openworld.Models;
+using UnityEngine;
+
 namespace Openworld.Scenes
+
 {
   public class CharacterScene : BaseScene
   {
+    public CharacterDetail character;
     protected override void Start()
     {
-        base.Start();
+      base.Start();
       uiManager.CloseMenu();
+    }
+
+    protected override void GetData()
+    {
+      var gameManager = GetGameManager();
+      var communicator = gameManager.GetCommunicator();
+      var player = gameManager.GetPlayer();
+      //get the character detail from the server, if character is not set, or 404 from server, show the menu
+      if (!(player.character is null) && !player.character.Equals(""))
+      {
+        GetGameManager().GetCommunicator().GetCharacterDetail(player.character, (CharacterDetailResponse resp) =>
+        {
+          Debug.Log(resp);
+          this.character = resp;
+        }, RequestException);
+      }
     }
   }
 }
